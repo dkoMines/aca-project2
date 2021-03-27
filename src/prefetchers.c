b4af3a733ab658dd090dd192ec8bb425d10e6166
dkoMines <dko@mymail.mines.edu>
2021-03-27 15:27:47 -0600

    send to windows

--- src/prefetchers.c
+++ src/prefetchers.c
@@ -32,7 +32,15 @@ uint32_t sequential_handle_mem_access(struct prefetcher *prefetcher,
                                       bool is_miss)
 {
     // TODO: Return the number of lines that were prefetched.
-    return 0;
+    int count = 0;
+    uint32_t target = address+cache_system->line_size;
+    for (int i=0; i < *(uint32_t *)prefetcher->data; i++){
+	if (cache_system_mem_access(cache_system, target, 'R', true) == 0){
+		count ++;
+	}
+	target += cache_system->line_size;
+    }
+    return count;
 }
 
 void sequential_cleanup(struct prefetcher *prefetcher)
@@ -49,6 +57,8 @@ struct prefetcher *sequential_prefetcher_new(uint32_t prefetch_amount)
 
     // TODO allocate any additional memory needed to store metadata here and
     // assign to sequential_prefetcher->data.
+    sequential_prefetcher->data = malloc(sizeof(uint32_t));
+    sequential_prefetcher->data = &prefetch_amount;
 
     return sequential_prefetcher;
 }
